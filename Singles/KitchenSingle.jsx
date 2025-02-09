5982e2ec13d9480bdc0aa09d09c4fdaaef550b47
RaaviMunisree <130855389+RaaviMunisree@users.noreply.github.com>
2025-02-09 12:59:35 +0530

    Add files via upload

---
+++ Singles/KitchenSingle.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { kitchenData } from '../data/kitchen';
+import Navbar from '../components/Navbar';
+import { useCart } from '../context/CartContext';
+
+const KitchenSingle = () => {
+    const {id} =useParams();
+    const {cartItem,addToCart}=useCart()
+    console.log(cartItem)
+ 
+    const product=kitchenData.find((item)=>item.id===id)
+  return (
+   <>
+   <Navbar />
+   <div className="ind-section">
+        <div className="ind-image">
+            <img src={product.image} alt="" />
+        </div>
+        <div className="ind-details space">
+            <div className="ind-company">
+            <h2>{product.brand}</h2>
+            </div>
+        <div className="ind-model space">
+            <h2>{product.model}</h2>
+        </div>
+        <div className="ind-price space">
+            <p>
+                {product.price}
+            </p>
+        </div>
+        <div className="ind-desc space">
+            <p>
+                {product.description}
+            </p>
+        </div>
+        <button className='addToCart' onClick={()=>addToCart(product)}>Add to cart</button>
+        </div>
+    </div>
+   </>
+  )
+}
+
+export default KitchenSingle
